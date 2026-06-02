9758ef7502950e157fcadcf1335e9b81d6b7790b
ArthursSchmidt <arthurifc0@gmail.com>
2026-06-02 11:14:31 -0300

    realocado images & link V

--- src/data/product.js
+++ src/data/product.js
@@ -7,7 +7,7 @@ export const produtos = [
     descricao:
       'Dá acesso a um shopping center exclusivo onde você pode comprar qualquer item que já foi vendido no mundo real, mesmo coisas raras, descontinuadas ou esgotadas.',
     preco: 129.9,
-    capa: '.scr/components/planeta_de_compras.png',
+    capa: '/images/planeta_de_compras.png',
   },
   {
     id: 2,
@@ -16,7 +16,7 @@ export const produtos = [
     descricao:
       'Uma obra clássica sobre postura profissional, organização e boas práticas no desenvolvimento de softwareUm frasco com 100 pílulas. Tomar uma cura instantaneamente qualquer doença ou ferimento físico.',
     preco: 149.9,
-    capa: '',
+    capa: '/images/pilulas_respiracao.png',
   },
   {
     id: 3,
@@ -25,7 +25,7 @@ export const produtos = [
     descricao:
       'Uma loção que, quando aplicada, faz com que qualquer pessoa que toque em você sinta um carinho profundo e imediato, como se fossem melhores amigos ou família.',
     preco: 89.9,
-    capa: '',
+    capa: '/images/skinship.png',
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ export const produtos = [
     raridade: 'B-30',
     descricao: 'Sentar nesta almofada por apenas uma hora por dia faz você perder peso de forma saudável e moldar o corpo ideal.',
     preco: 79.9,
-    capa: 'https://covers.openlibrary.org/b/isbn/9781091210092-L.jpg',
+    capa: '/images/almofada_emagrecimento.png',
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ export const produtos = [
     descricao:
       'Uma câmera que não tira fotos de imagens, mas sim da sua memória atual, permitindo que você a visualize perfeitamente ou recupere lembranças esquecidas.',
     preco: 20.90,
-    capa: '',
+    capa: '/images/memoria_fotografica.png',
   },
   {
     id: 6,
@@ -50,7 +50,7 @@ export const produtos = [
     raridade: 'S-6',
     descricao: 'Uma maquete que se transforma em uma mansão de luxo de 20 quartos, com direito a mordomo, chef de cozinha e spa inclusos, contanto que você pague o "aluguel" (que no jogo é simbólico).',
     preco: 199.9,
-    capa: '',
+    capa: '/images/mansao_aluguel.png',
   },
   {
     id: 7,
@@ -59,7 +59,7 @@ export const produtos = [
     descricao:
       'Dormir dentro desta concha gigante garante que você acorde se sentindo 100% revigorado, não importa o quão cansado estava ou quão poucas horas tenha dormido.',
     preco: 55.99,
-    capa: '',
+    capa: '/images/concha_despertar.png',
   },
   {
     id: 8,
@@ -67,6 +67,6 @@ export const produtos = [
     raridade: 'B-66',
     descricao: 'Um bilhete que permite que você vá para qualquer lugar do mundo instantaneamente e retorne ao ponto de partida assim que o tempo estipulado acabar.',
     preco: 27.98,
-    capa: './scr/components/products/bilhete_viagem.png',
+    capa: '/images/bilhete_viagem.png',
   },
 ]
